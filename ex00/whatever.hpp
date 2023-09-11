e263596084a9a3ca0fa4d1a876ff31568dc07538
Unai Martin marante <umartin-@c3r5s4.42urduliz.com>
2023-09-11 17:51:26 +0200

    ex00

---
+++ ex00/whatever.hpp
@@ -0,0 +1,35 @@
+#ifndef WHATVER_HPP
+# define WHATVER_HPP
+
+#include <iostream>
+#include <string>
+
+template <typename T>
+
+void swap(T &a, T &b)
+{
+	T tmp = a;
+	a = b;
+	b = tmp;
+}
+
+template <typename T>
+
+T min(T a, T b)
+{
+	if (a < b)
+		return a;
+	return b;
+}
+
+template <typename T>
+
+T max(T a, T b)
+{
+	if (a > b)
+		return a;
+	return b;
+}
+
+
+#endif
